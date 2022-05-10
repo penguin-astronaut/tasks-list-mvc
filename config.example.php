b62bc27ef132e4c8a11eaf2cdb6c53bba9a1fe4f
Vadim Kasatkin <penguin2804@gmail.com>
2022-05-10 13:40:04 +0400

    feat: example cofing file

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => '127.0.0.1',
+        'db_name'   => 'tasks',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8'
+    ]
+];
